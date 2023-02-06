9bb3f117f7a6fce8434ca0e9e0b2103fd845343d
Nourhan El Sherif <nourhane@bu.edu>
2023-02-05 22:59:35 -0500

    passed autograder test cases

--- fun.cpp
+++ fun.cpp
@@ -2,36 +2,45 @@
 #include <iostream> 
 #include <unordered_map>
 #include <string>
+// #include <chrono>
+// using namespace std::chrono;
 
-unsigned int fun_helper(unsigned int, unsigned int, std::unordered_map<std::string, int>*);
+
+unsigned int fun_helper(unsigned int, unsigned int, std::unordered_map<std::string, int>*); // function header
+
+std::unordered_map<std::string, int> results; // global
 
 unsigned int fun(unsigned int x, unsigned int y) { 
-    std::unordered_map<std::string, int> map;
-    return fun_helper(x,y, &map);
+    return fun_helper(x,y, &results);
 }
 
-/* your helper function here if you have any */
 unsigned int fun_helper(unsigned int x, unsigned int y, std::unordered_map<std::string, int> * temp_map){
     std::string space = " "; 
-    if(temp_map->find(std::to_string(x) + space + std::to_string(y)) == temp_map->end()) { // not found: calculate and save value
-        if(x == 0 && y == 0)
-            temp_map->insert({std::to_string(x) + space + std::to_string(y), 1}); // insert({key, element})
-            //return 1;
+    std::unordered_map<std::string,int>::iterator iter = temp_map->find(std::to_string(x) + space + std::to_string(y)); // iterator searches for input term
+    if ( iter != temp_map->end() ) // found
+        return iter->second;
+    else { // key not found in map: calculate and save value
+         if(x == 0 && y == 0)
+            return 1; // base case
         else if(x == 0 && y !=0)
-            return fun(1, y-1);
+            temp_map->insert({std::to_string(x) + space + std::to_string(y), fun_helper(1, y-1, &results)}); // insert({key, element})
         else if(y == 0 && x !=0)
-            temp_map->insert({std::to_string(x) + space + std::to_string(y), x+1});
-            //return x + 1;
+            return x + 1; // base case
         else
-            return fun(x-1, fun(x-1, y-1));
+            temp_map->insert({std::to_string(x) + space + std::to_string(y), fun_helper(x-1, fun_helper(x-1, y-1, &results), &results)}); // recursion case
         }
-    // if found, or after val calculated
-    return temp_map->at(std::to_string(x) + space + std::to_string(y));
+    return temp_map->at(std::to_string(x) + space + std::to_string(y)); // return saved value
 }
 
 // int main(){
-//     int x = 10;
-//     int y = 5;
+//     int x = 100;
+//     int y = 100;
+// //     auto start = high_resolution_clock::now();
 //     std::cout << fun(x,y) << std::endl;
+// // timing the program
+// //     auto stop = high_resolution_clock::now();
+// //     auto duration = duration_cast<microseconds>(stop - start);
+// //     std:: cout << duration.count() << std:: endl;
+
 //     return 0;
 // }
